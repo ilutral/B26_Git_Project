a794aae0618ac57393ce8b7423f20bf95bf74b76
ilutral <95429277+ilutral@users.noreply.github.com>
2022-03-28 16:21:54 -0500

    Create JamesBond.java

---
+++ src/day0/JamesBond.java
@@ -0,0 +1,16 @@
+package day0;
+
+public class James Bond{
+
+  public static void main (String[] args){
+  
+    System.out.println("James Bond hacked the project");
+    
+    
+    
+  
+  
+  }
+}
+  
+  
